56b625f641a2bd3980422daa4d4476a2de7672ef
Aarsh <aarshpatel2101@gmail.com>
2024-03-22 17:44:03 -0500

    vercel setup

--- website/website/settings.py
+++ website/website/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-6@=&_%6zke-=nz!71@7j0es%3+p$ik5+p@6l!18szyfi(^4m!n
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
 
 
 # Application definition
